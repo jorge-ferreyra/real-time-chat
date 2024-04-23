a0426c42d8112640203572cbca976e1a62646bef
Jorge Ferreyra <jorxfer@gmail.com>
2024-04-23 11:59:22 -0300

    Adding users to messages

--- server/index.js
+++ server/index.js
@@ -26,7 +26,8 @@ const db = createClient({
 await db.execute(`
   CREATE TABLE IF NOT EXISTS messages (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    content TEXT
+    content TEXT,
+    user TEXT
   )
 `)
 
@@ -39,28 +40,29 @@ io.on('connection', async (socket) => {
 
   socket.on('chat message', async (msg) => {
     let result
+    let username = socket.handshake.auth.username ?? 'anonymous'
     try {
       result = await db.execute({
-        sql: 'INSERT INTO messages (content) VALUES (:message)',
-        args: { message: msg }
+        sql: 'INSERT INTO messages (content, user) VALUES (:msg, :username)',
+        args: { msg, username }
       })
     } catch (e) {
       console.error(e)
       return
     }
 
-    io.emit('chat message', msg, result.lastInsertRowid.toString())
+    io.emit('chat message', msg, result.lastInsertRowid.toString(), username)
   })
 
   if (!socket.recovered) {
     try {
       const result = await db.execute({
-        sql: 'SELECT id, content FROM messages WHERE id > ?',
+        sql: 'SELECT id, content, user FROM messages WHERE id > ?',
         args: [socket.handshake.auth.serverOffset ?? 0]
       })
 
       result.rows.forEach(row => {
-        socket.emit('chat message', row.content, row.id.toString())
+        socket.emit('chat message', row.content, row.id.toString(), row.user)
       })
     } catch(e) {
       console.error(e)
